19dd02c93b8b03c5cf230aea4766c2c245731347
daghb <dagh@baardsen.org>
2017-02-08 09:47:34 +0100

    Laget spesial-byggefil for dagh

--- rebuild.dagh.bat
+++ rebuild.dagh.bat
@@ -1,4 +1,4 @@
-call npm run build
+rem call npm run build
 call addjson.cmd
 call ..\kibana-5.1.2\bin\kibana-plugin.bat remove logtrail
 call ..\kibana-5.1.2\bin\kibana-plugin.bat install file://c:/dev/sandbox/logtrail-5/build/logtrail-0.1.7.zip
